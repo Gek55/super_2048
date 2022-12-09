c074ab8d72acb8ce8af4efe25e25af1b3ea2891e
Gek55 <111584898+Gek55@users.noreply.github.com>
2022-12-09 20:32:29 +0300

    Add files via upload

--- Build/UnityLoader.js
+++ Build/UnityLoader.js
@@ -1,15 +1,6 @@
 var UnityLoader = UnityLoader || {
     compatibilityCheck: function(e, t, r) {
-        UnityLoader.SystemInfo.hasWebGL ? UnityLoader.SystemInfo.mobile ? e.popup("Please note that Unity WebGL is not currently supported on mobiles. Press OK if you wish to continue anyway.", [{
-            text: "OK",
-            callback: t
-        }]) : ["Firefox", "Chrome", "Safari"].indexOf(UnityLoader.SystemInfo.browser) == -1 ? e.popup("Please note that your browser is not currently supported for this Unity WebGL content. Press OK if you wish to continue anyway.", [{
-            text: "OK",
-            callback: t
-        }]) : t() : e.popup("Your browser does not support WebGL", [{
-            text: "OK",
-            callback: r
-        }])
+       t();
     },
     Blobs: {},
     loadCode: function(e, t, r) {
